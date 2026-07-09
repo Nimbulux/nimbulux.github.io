0e37367d9cd10ea2f0e44c7eaddd334528f16c45
Nimbulux_ <2121402422@qq.com>
2026-07-10 03:08:10 +0800

    保存

--- 404.js
+++ 404.js
@@ -1,4 +1,7 @@
 document.title = "正在加载";
 
-document.title = "404未找到";
+const link = location.pathname;
+
 document.getElementById("NotFoundShow").classList.remove("hide")
+document.getElementById("LinkShow").textContent = link;
+document.title = "404未找到";
